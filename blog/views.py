8a91243e5a8eed68c33f78bc0bc625c330ba9139
Alex Amedia <vs8pnpktb4@privaterelay.appleid.com>
2025-12-03 11:28:07 -0700

    Linking the Pages; Adding the "Single Post" Page

--- blog/views.py
+++ blog/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def starting_page(request):
-    return render(request, 'blog/index.html')
+    return render(request, "blog/index.html")
 
 def posts(request):
-    return render(request, 'blog/all-posts.html')
+    return render(request, "blog/all-posts.html")
 
 def post_detail(request, slug):
-    pass
+    return render(request, "blog/post-detail.html")
